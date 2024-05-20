1ad5092219bcb6e7fcd7222b5703d20224b25f6d
therahuljha16 <therahuljha16@gmail.com>
2024-05-19 21:15:07 -0500

    Delete ASSIGNMENT/PART1 directory

--- ASSIGNMENT/PART1/Part2/employee-microservice/src/main/java/com/example/employee/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.employee;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface EmployeeRepository extends MongoRepository<Employee, String> {
-    Employee findByEmployeeId(int employeeId);
-}
--- ASSIGNMENT/PART1/employee-microservice/src/main/java/com/example/employee/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.employee;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    Employee findByEmployeeId(int employeeId);
-}
-
